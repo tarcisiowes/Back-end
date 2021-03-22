5e5c46c2621649c2b79b3c092a64a31327ddae5e
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-03-22 11:12:00 -0300

    Add files via upload

--- back-m01-a04/m01-a04-casa-16/index.js
+++ back-m01-a04/m01-a04-casa-16/index.js
@@ -1,2 +1,10 @@
 const original = [5, 7, 13, 17, 26, 34, 118, 245];
 
+let newlist = []
+for (let i =0; i < original.length; i++){
+    
+    if(original[i] >=10 && original[i] <= 20 || original[i] > 100) {
+        newlist.push(original[i])
+    }
+}
+console.log(newlist)
