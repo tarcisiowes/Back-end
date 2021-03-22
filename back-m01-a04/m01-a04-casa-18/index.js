e9d156f322b257d4f5f7a311c063316f7a958fe6
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-03-22 12:34:02 -0300

    Add files via upload

--- back-m01-a04/m01-a04-casa-18/index.js
+++ back-m01-a04/m01-a04-casa-18/index.js
@@ -1,2 +1,11 @@
 const disjuntores = [false, false, true, false, false, true, false, false];
 
+for(let i = 0; i < disjuntores.length; i++) {
+    const ligado = disjuntores[i]
+    if( ligado ) {
+        console.log(i)
+    }
+    
+}
+    
+
