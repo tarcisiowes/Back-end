9a3e316547121be2a239cbe6cfd116a5bda6cbc3
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-03-22 11:04:17 -0300

    Add files via upload

--- back-m01-a04/m01-a04-casa-15/index.js
+++ back-m01-a04/m01-a04-casa-15/index.js
@@ -1,2 +1,13 @@
 const numeros = [8, 11, 4, 1];
 
+let maior = 0
+let menor = 999
+
+for(let i = 0; i < numeros.length; i++) {
+    if (numeros[i] > maior) {
+        maior = numeros[i]
+    } if ( numeros[i] < menor) {
+        menor = numeros[i]
+    }
+}
+console.log(maior-menor)
