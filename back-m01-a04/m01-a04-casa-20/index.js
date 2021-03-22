37dc49f3fc3fa9ef7269876a58c2db004a165109
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-03-22 13:24:53 -0300

    Add files via upload

--- back-m01-a04/m01-a04-casa-20/index.js
+++ back-m01-a04/m01-a04-casa-20/index.js
@@ -2,3 +2,46 @@ const original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const pares = [];
 const impares = [];
 
+for(let i = 0; i < original.length; i++) {
+    
+    
+    original[i] % 2 === 0 ? pares.push(original[i]) : impares.push(original[i])
+
+}
+console.log(pares)
+console.log(impares)
+
+// ===========================================================================
+
+let x = 0
+
+const pares2 = []
+const impares2 = []
+
+while(x < original.length){
+    
+    if(original[x] % 2 === 0) {
+        pares2.push(original[x])
+    } else {
+        impares2.push(original[x])
+    }
+    x++
+}   
+console.log(pares2)
+console.log(impares2)
+
+// ============================================================================
+
+const pares3 = []
+const impares3 = []
+for (let n of original) {
+
+    if( n % 2 === 0) {
+        pares3.push(n)
+    } else {
+        impares3.push(n)
+    }
+
+}
+console.log(pares3)
+console.log(impares3)
