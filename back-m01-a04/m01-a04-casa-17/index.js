7329caface8347b496af5d4302feedb7727258e9
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-03-22 11:21:21 -0300

    Add files via upload

--- back-m01-a04/m01-a04-casa-17/index.js
+++ back-m01-a04/m01-a04-casa-17/index.js
@@ -1,3 +1,11 @@
 const filaDeDentro = ["Jose", "Maria", "Joao"];
 const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
 
+for(let i = 0; i < filaDeFora.length;i++) {
+    if( filaDeDentro.length < 5) {
+        filaDeDentro.push(filaDeFora[0])
+        filaDeFora.shift()
+    }
+}
+console.log(filaDeDentro)
+console.log(filaDeFora)
