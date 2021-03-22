394dffaf7fc0be2823756c0b66dc1c8d5efb5520
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-03-22 13:00:58 -0300

    Add files via upload

--- back-m01-a04/m01-a04-casa-19/index.js
+++ back-m01-a04/m01-a04-casa-19/index.js
@@ -1,3 +1,14 @@
 const arrayA = [5, 32, 3, 44, 1];
 const arrayB = [57, 4, 21, 2, 13];
 
+let arrayC = []
+
+
+for(let i = 0; i < arrayA.length; i++) {
+    
+    let position = i
+
+    arrayA[i] < arrayB[position] ? arrayC.push(arrayA[i]) : arrayC.push(arrayB[position])
+
+}
+console.log(arrayC)
