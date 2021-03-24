c6351828ddca9fcbd903c714b69751c896962685
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-03-24 12:42:36 -0300

    Add files via upload

---
+++ back-m01-a05/m01-a05-classe-07/questao07.js
@@ -0,0 +1,27 @@
+const prova = {
+    aluno: "João",
+    materia: "Português",
+    valor: 10,
+    questoes: [
+        {
+            resposta: "a",
+            correta: "b"
+        },
+        {
+            resposta: "c",
+            correta: "c"
+        },
+        {
+            resposta: "e",
+            correta: "b"
+        },
+        {
+            resposta: "b",
+            correta: "b"
+        },
+        {
+            resposta: "c",
+            correta: "c"
+        }
+    ]
+};
