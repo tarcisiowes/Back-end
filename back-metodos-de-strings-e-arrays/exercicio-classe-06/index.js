dd4c3755719af6bf2ffb0331825f296ff1ec7886
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-03-25 20:12:24 -0300

    Add files via upload

--- back-metodos-de-strings-e-arrays/exercicio-classe-06/index.js
+++ back-metodos-de-strings-e-arrays/exercicio-classe-06/index.js
@@ -1,2 +1,39 @@
 const cpf = "12345678900";
-const cnpj = "12345678900";
+const cnpj = "12345678000199";
+
+//usar splice
+
+const cpfvalido = cpf.length === 11
+const cnpjvalido = cnpj.length === 14
+
+
+
+function formatarCpf(cpf) {
+    let numcpf = cpf.split(""); 
+
+    if(cpfvalido) {
+        numcpf.splice(3,0,".");
+        numcpf.splice(7,0,".");
+        numcpf.splice(11,0,"-");
+        console.log(numcpf.join(""))
+    } else {
+        console.log("CPF Inválido")
+    }
+}
+formatarCpf(cpf)
+
+function formatarCnpj(cnpj) {
+    let numcnpj = cnpj.split(""); 
+
+    if(cnpjvalido) {
+        numcnpj.splice(2,0,".");
+        numcnpj.splice(6,0,".");
+        numcnpj.splice(10,0,"/");
+        numcnpj.splice(15,0,"-");
+        console.log(numcnpj.join(""))
+    } else {
+        console.log("CNPJ Inválido")
+    }
+}
+formatarCnpj(cnpj)
+
