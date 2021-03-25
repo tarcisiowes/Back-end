a0cf3b30af9cee235eca1aa89b9605507b6dfe68
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-03-25 19:32:29 -0300

    Add files via upload

--- back-metodos-de-strings-e-arrays/exercicio-classe-05/index.js
+++ back-metodos-de-strings-e-arrays/exercicio-classe-05/index.js
@@ -1,1 +1,14 @@
-const comentario = "Esse COVID é muito perigoso!";
+const comentario = "Esse  é muito perigoso!";
+
+const comentarioArray = comentario.split(" ")
+const proibido = ["covid", "Covid","COVID","Pandemia","PANDEMIA","pandemia"]
+let tem = 0
+for(let palavra of comentarioArray){
+    if (proibido.includes(palavra)) {
+        tem += 1        
+    }    
+
+}
+
+console.log(tem > 0? "Comentário bloqueado por conter palavras proibidas" : "Comentário autorizado")
+
