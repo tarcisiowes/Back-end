ff2fc2328d4169ae4a66e4ffac47802b14387e24
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-03-25 20:25:28 -0300

    Add files via upload

--- back-metodos-de-strings-e-arrays/exercicio-classe-07/index.js
+++ back-metodos-de-strings-e-arrays/exercicio-classe-07/index.js
@@ -1,1 +1,15 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
+
+let low = texto.toLowerCase(); 
+// console.log(low);
+
+
+let link = low;
+let linkAnterior = "";
+
+while (link !== linkAnterior) {
+    linkAnterior = link;
+    link = link.replace(" " , "-");
+
+}
+console.log(link)
