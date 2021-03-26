9bbc25c4baceb49285952a7d5a453da504a0b340
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-03-25 21:47:03 -0300

    Add files via upload

--- back-metodos-de-strings-e-arrays/exercicio-classe-08/index.js
+++ back-metodos-de-strings-e-arrays/exercicio-classe-08/index.js
@@ -2,3 +2,26 @@ let identificador = "123";
 let nome = "José silva costa";
 let email = "      jose@email.com  ";
 let tags = ['financeiro', 'administrativo', 'geral'];
+
+const idvalido = `${identificador}`.padStart(6,"0")
+
+const emailvalido = email.trim()
+
+const tagvalida = tags.join()
+
+const nomecompleto = nome.split(" ")
+
+
+for(let i  = 0; i < nomecompleto.length; i++) {
+   
+    nomecompleto[i] = nomecompleto[i].replace(nomecompleto[i][0], nomecompleto[i][0].toUpperCase());
+    
+}
+const nomevalido = nomecompleto.join( " ")
+
+console.log(idvalido)
+console.log(nomevalido)
+console.log(emailvalido)
+console.log(tagvalida)
+
+
