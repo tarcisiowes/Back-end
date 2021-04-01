225dc34c660c25498dbef88070603e21f29109a5
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-04-01 20:34:29 -0300

    Add files via upload

--- back-reduce-every-some/classe-06/index.js
+++ back-reduce-every-some/classe-06/index.js
@@ -1,1 +1,5 @@
-const palavras = ['livro', 'caneta', 'sol', 'carro', 'orelha'];
+const numeros = [0, 122, 4, 6, 7, 8, 44];
+
+const ehValido = numeros.every(x => x % 2 === 0);
+
+console.log(ehValido ? "Array válido" : "Array inválido")
