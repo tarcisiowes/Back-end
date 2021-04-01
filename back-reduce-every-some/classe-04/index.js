2a223a5a5a61cee99468771e87aac6a84538b406
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-04-01 20:18:17 -0300

    Add files via upload

--- back-reduce-every-some/classe-04/index.js
+++ back-reduce-every-some/classe-04/index.js
@@ -1,1 +1,5 @@
 const palavras = ['livro', 'caneta', 'sol', 'carro', 'orelha'];
+
+const temMaisQue5Letras = palavras.some(x => x.length > 5);
+
+console.log(temMaisQue5Letras ? "Existe palavra inválida" : "Nao existe palavra inválida")
