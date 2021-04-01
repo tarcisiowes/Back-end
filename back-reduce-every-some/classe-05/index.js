7e80cdef9dc3d3c999aac9cee3601bc85ca0a8a3
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-04-01 20:26:12 -0300

    Add files via upload

--- back-reduce-every-some/classe-05/index.js
+++ back-reduce-every-some/classe-05/index.js
@@ -1,1 +1,5 @@
 const palavras = ["arroz", "feijão", "carne", "cerveja", "macarrão"];
+
+const temBebida = palavras.some(x => x === "cerveja" || x === "vodka" );
+
+console.log(temBebida ? "Revise sua lista, joão. Possui bebida com venda proibida!" : "Tudo certo, vamos as compras!")
