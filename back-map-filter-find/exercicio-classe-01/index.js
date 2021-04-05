5c7c6670035bfc0358845a1f9425e40d5fae13e5
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-04-05 12:04:03 -0300

    Add files via upload

--- back-map-filter-find/exercicio-classe-01/index.js
+++ back-map-filter-find/exercicio-classe-01/index.js
@@ -1,1 +1,6 @@
 const cidades = ['Salvador', 'São Paulo', 'Brasilia', 'Recife', 'Rio de Janeiro'];
+
+const cidadesFiltradas = cidades.filter(x => x.length <= 8);
+
+
+console.log(cidadesFiltradas.join());
